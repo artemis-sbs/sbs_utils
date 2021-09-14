4f16c5b1788c80fef892f912cdd8f78b4416a69f
dougreichard <doug.reichard+gitlab@gmail.com>
2021-09-14 18:54:52 -0400

    wrong name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="demopackage",
+    name="sbs_example",
     version="1.0.0",
     author="Doug Reichard",
     author_email="demo@email.com",
