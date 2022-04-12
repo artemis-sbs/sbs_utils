0dd514e89148802c559c840bea7a019398eb72a8
dougreichard <doug.reichard+gitlab@gmail.com>
2022-04-12 19:02:47 -0400

    Renamed repo

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sbs_scatter",
+    name="sbs_utils",
     version="1.0.0",
     author="Doug Reichard",
     author_email="demo@email.com",
     description="A small demo package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dougreichard/sbs_scatter",
+    url="https://github.com/dougreichard/sbs_utils",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
