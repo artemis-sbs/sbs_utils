af76a430b859c8f4d445f9cc641b3cd9b34d2750
dougreichard <doug.reichard+gitlab@gmail.com>
2022-09-06 14:44:25 -0400

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sbs_utils",
-    version="1.0.0",
+    version="0.2.0",
     author="Doug Reichard",
     author_email="demo@email.com",
     description="A small demo package",
