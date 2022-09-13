e1afb01bffc8408e05abfb7dcc805608abbb94f4
dougreichard <doug.reichard+gitlab@gmail.com>
2022-09-13 16:30:27 -0400

    setup

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dougreichard/sbs_utils",
-    packages=setuptools.find_packages(
-        where='sbs_utils'
-    ),
+    packages=setuptools.find_packages(),
     
     classifiers=(
         "Programming Language :: Python :: 3",
