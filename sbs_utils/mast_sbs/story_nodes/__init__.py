f0679b89a70021d338af23b37a8bce23176d4159
dougreichard <doug.reichard+github@gmail.com>
2025-02-22 17:36:53 -0500

    removed cards

--- sbs_utils/mast_sbs/story_nodes/__init__.py
+++ sbs_utils/mast_sbs/story_nodes/__init__.py
@@ -3,17 +3,8 @@
 # CARDS
 from .inline_route import InlineRoute
 
-# from .card_admiral import AdmiralCardLabel
-# from .card_character import CharacterCardLabel
-# from .card_deck import DeckCardLabel
-# from .card_fleet import FleetCardLabel
+
 from .card_map import MapCardLabel
-# from .card_objective import ObjectiveLabel
-# from .card_player import PlayerCardLabel
-# from .card_prefab import PrefabCardLabel
-# from .card_tile import TileCardLabel
-# from .card_unit import UnitCardLabel
-# from .card_upgrade import UpgradeLabel
 ##########################
 from .define_format import DefineFormat # must be before Button and comms message
 from .weighted_text import WeightedText
