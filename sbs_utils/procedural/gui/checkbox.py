32ecbb68d83f6c489c10cfe0d6297d18a04fc2b4
dougreichard <doug.reichard+github@gmail.com>
2025-03-06 11:42:41 -0500

    Checkbox default value

--- sbs_utils/procedural/gui/checkbox.py
+++ sbs_utils/procedural/gui/checkbox.py
@@ -19,13 +19,16 @@ def gui_checkbox(msg, style=None, var=None, data=None):
         return None
     tag = page.get_tag()
     msg = task.compile_and_format_string(msg)
+    value = False
+    if var is not None:
+        value = task.get_variable(var, False)
 
-    layout_item = Checkbox(tag, msg)
+    layout_item = Checkbox(tag, msg, value)
     layout_item.data = data
     if var is not None:
         layout_item.var_name = var
         layout_item.var_scope_id = task.get_id()
-        layout_item.update_variable()
+    #     layout_item.update_variable()
 
     apply_control_styles(".checkbox", style, layout_item, task)
     # Last in case tag changed in style
