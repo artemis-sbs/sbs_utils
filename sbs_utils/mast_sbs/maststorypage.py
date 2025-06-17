44ce976446c8c04519c38aaa85d973c3f4e48a71
dougreichard <doug.reichard+github@gmail.com>
2025-06-17 18:21:25 -0400

    gui_button jump vs subtask. New  gui ends gui_sub tasks

--- sbs_utils/mast_sbs/maststorypage.py
+++ sbs_utils/mast_sbs/maststorypage.py
@@ -234,6 +234,9 @@ class StoryPage(Page):
         self.pending_gui = False
         from ..procedural.gui.property_listbox import gui_reset_variables
         gui_reset_variables(self.gui_task)
+        for sub_task in self.gui_task.sub_tasks:
+            if sub_task.has_role("end_on_new_gui"):
+                sub_task.end()
 
 
 
